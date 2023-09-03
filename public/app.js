c93562bbdceebe1b9a027ebf26699cd732e5492f
Iamneo <techadmin@iamneo.ai>
2023-09-03 18:15:47 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -276,7 +276,54 @@ const ticTacToe = (element, index) => {
             flag = 1;
         }
     }
-    
+    function myfunc_8(){
+        if(flag == 1){
+            document.getElementById("b6").value = "X";
+            document.getElementById("b6").disabled = true;
+            flag = 0;
+        }
+        else{
+            document.getElementById("b6").value = "O";
+            document.getElementById("b6").disabled = true;
+            flag = 1;
+        }
+    }
+    function myfunc_9(){
+        if(flag == 1){
+            document.getElementById("b7").value = "X";
+            document.getElementById("b7").disabled = true;
+            flag = 0;
+        }
+        else{
+            document.getElementById("b7").value = "O";
+            document.getElementById("b7").disabled = true;
+            flag = 1;
+        }
+    }
+    function myfunc_10(){
+        if(flag == 1){
+            document.getElementById("b8").value = "X";
+            document.getElementById("b8").disabled = true;
+            flag = 0;
+        }
+        else{
+            document.getElementById("b8").value = "O";
+            document.getElementById("b8").disabled = true;
+            flag = 1;
+        }
+    }
+    function myfunc_11(){
+        if(flag == 1){
+            document.getElementById("b9").value = "X";
+            document.getElementById("b9").disabled = true;
+            flag = 0;
+        }
+        else{
+            document.getElementById("b9").value = "O";
+            document.getElementById("b9").disabled = true;
+            flag = 1;
+        }
+    }
 };
 
     /*
